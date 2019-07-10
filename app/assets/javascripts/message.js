ba89405a474c9f33db457058bb96afee1fba640a
gh-d <nd0714_0722@yahoo.co.jp>
2019-07-10 20:55:53 +0900

    repaire

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -36,7 +36,7 @@ $(function(){
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html)
-    $('#message_content').val('')
+    $('#message_content')[0].reset()
     $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
   })
   .fail(function(){
