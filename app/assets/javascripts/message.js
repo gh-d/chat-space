779d322cdf43f8eb6c305e8c8b5833b8865cbbbe
gh-d <nd0714_0722@yahoo.co.jp>
2019-07-12 12:26:18 +0900

    revert

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){
   function buildHTML(message){
+<<<<<<< HEAD
     var image_url = (message.image_url)? `<image class="lower-message_image" src="${message.image_url}">`:"";
     var html = `<div class="message" id='${message.id}'>
                   <div class="upper-message" >
@@ -18,6 +19,88 @@ $(function(){
     return html;
   }
   
+=======
+    // var image = (message.image.url) ? `<p><img src="${message.image.url}"></p>` : "";
+    
+      if (message.content && message.image.url) {
+        //data-idが反映されるようにしている
+        var html = `<div class="message" data-id= &{message.id}  > 
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at} 
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            <img src=" message.image.url  " class="lower-message__image" >
+          </div>
+        </div>`
+      } else if (message.content) {
+        //同様に、data-idが反映されるようにしている
+        var html = `<div class="message" data-id=  ${message.id} >
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name} 
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at} 
+            </div>
+          </div>
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      } else if (message.image.url) {
+        //同様に、data-idが反映されるようにしている
+        var html = `<div class="message" data-id= ${message.id} >
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name }
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="lower-message">
+            <img src=" ${message.image.url} " class="lower-message__image" >
+          </div>
+        </div>`
+      };
+      return html;
+    };
+   
+  
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url =$(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('#message_image').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('エラー');
+    });
+  })
+>>>>>>> parent of ba85cce... repair2
 
   var reloadMessages = function(){
     last_message_id = $(".message:last").data("id");
