8eddd27902b37e4ebb540f2108e35940e04828b4
gh-d <nd0714_0722@yahoo.co.jp>
2019-07-13 17:31:40 +0900

    update url

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "https://github.com/gh-d/chat-space.git"
+set :repo_url, "git@github.com:gh-d/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<ローカルPCのEC2インスタンスのSSH鍵(pem)へのパス>']
+                  keys: ['~/.ssh/newpair.pem']
 
                 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
 server '52.69.65.246', user: 'ec2-user', roles: %w{app db web}
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
