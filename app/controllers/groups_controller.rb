a1ecfd40445fde9c4a4721c8fd0f561c0221e899
gh-d <nd0714_0722@yahoo.co.jp>
2019-07-14 17:18:39 +0900

    repair5

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+ before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
+    binding.pry
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
