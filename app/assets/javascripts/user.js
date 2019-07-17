c9fa4c40172696a79dba4462c92e309dbb3c2c47
gh-d <nd0714_0722@yahoo.co.jp>
2019-07-17 17:21:13 +0900

    group-edit

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -16,14 +16,13 @@ $(function() {
   search_list.append(html);
   }
 
-  var member_list = $("#chat-group-users");
+  var member_list = $(".chat-group-user.clearfix#unti");
 
   function addUser(userId,userName) {
-  var html = `<div id='chat-group-users'>
-                <div class='chat-group-user clearfix js-chat-member' id='${userId}'>
-                  <input name='group[user_ids][]' type='hidden' value='${userId}'>
-                    <p class='chat-group-user__name'>${userName}</p>
-                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+  var html = `<div class="chat-group-user clearfix js-chat-member" id="${userId}">
+                <input name="group[${userId}][]" type="hidden" value="${userId}">
+                <p class="chat-group-user__name">${userName}</p>
+                <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
               </div>`;
   member_list.append(html);
     }
